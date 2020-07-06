113a994ed0f974918eb5d410602713bdf4420969
hpc <philipchanhk626@gmail.com>
2020-07-06 15:48:42 -0400

    rendering data fetched, handling pending queries with if

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -5,18 +5,34 @@ import { graphql } from 'react-apollo'
 function SongList(props) {
 
   console.log(props)
+
+  const renderSongs = () => {
+    return props.data.songs.map(song => {
+      return (
+        <li key={song.id}>
+          {song.title}
+        </li>
+      )
+    })
+  }
   
-  return (
-    <div>
-      SongList
-    </div>
-  )
+  if (props.data.loading) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div>
+        {renderSongs()}
+      </div>
+    )
+  }
+
 }
 
 
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
